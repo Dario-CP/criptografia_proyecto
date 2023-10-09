33235128ea71a6d261e332b9c3c1e005152d98bd
RaulCarrero02 <100451286@alumnos.uc3m.es>
2023-10-09 18:01:43 +0200

    Comentarios

--- src/main/Python/pwd_manager/main.py
+++ src/main/Python/pwd_manager/main.py
@@ -16,75 +16,81 @@ global window_register
 global window_user
 
 
-
-
-
-# ventana register
+# FUNCIÓN QUE CREA LA VENTANA REGISTRAR
 def register_window():
     """Sign up window"""
     # ----VENTANA REGISTRAR----
     Label(window_register, text="", bg=background_color, fg='#ffF').pack()
-    Button(window_register, text="Log in", height="2", width="30", bg="#FFFFFF", command=login_window).pack()
+    Button(window_register, text="Back", height="2", width="30", bg="#FFFFFF", command=back).pack()
     Label(window_register, text="", bg=background_color, fg='#ffF').pack()
     Label(window_register, text="Please enter details below to login", fg='#ffF', bg=background_color).pack()
     Label(window_register, text="", bg=background_color).pack()
-    # FORMULARIO DATOS REGISTER
-    # username
+    # FORMULARIO REGISTRO DE DATOS
+    # Usuario
     Label(window_register, bg=background_color, fg='#ffF', text="Username * ").pack()
     Entry(window_register, textvariable=username).pack()
-    # password
+    # Contraseña
     Label(window_register, bg=background_color, fg='#ffF', text="Password * ").pack()
     Entry(window_register, textvariable=password, show='*').pack()
     Label(window_register, text="", bg=background_color).pack()
-    # boton register
+    # Boton de registrar
     Button(window_register, text="sign up", height="2", width="30", bg="#FFFFFF", command=register_user).pack()
     window_register.pack()
-    # Hide log in window
+    # Esconder la ventana log in
     if window_login:
-        window_login.forget()
+        window_login.destroy()
         window_home.forget()
 
-#ventana login
+# FUNCIÓN QUE CREA LA VENTANA LOG IN
 def login_window():
     """Log in window"""
     # ----VENTANA LOGIN----
     Label(window_login, text="", bg=background_color, fg='#ffF').pack()
-    Button(window_login, text="Register", height="2", width="30", bg="#FFFFFF", command=register_window).pack()
+    Button(window_login, text="Back", height="2", width="30", bg="#FFFFFF", command=back).pack()
     Label(window_login, text="", bg=background_color, fg='#ffF').pack()
     Label(window_login, text="Please enter details below to login", fg='#ffF', bg=background_color).pack()
     Label(window_login, text="", bg=background_color).pack()
     # FORMULARIO DATOS LOG IN
-    # username
+    # Usuario
     Label(window_login, bg=background_color, fg='#ffF', text="Username * ").pack()
     Entry(window_login, textvariable=username).pack()
-    # password
+    # Contraseña
     Label(window_login, bg=background_color, fg='#ffF', text="Password * ").pack()
     Entry(window_login, textvariable=password, show='*').pack()
     Label(window_login, text="", bg=background_color).pack()
-    # boton log in
+    # Boton de log in
     Button(window_login, text="log in", height="2", width="30", bg="#FFFFFF", command=login_user).pack()
     window_login.pack()
+    # Esconder la ventana registrar
     if window_register:
-        window_register.forget()
+        window_register.destroy()
         window_home.forget()
 
+# FUNCIÓN QUE CREA LA VENTANA DEL USUARIO
 def user_window():
+    """User window"""
     # ----VENTANA DE USUARIO----
-    window_user = Frame(window_principal)
-    window_user.config(width=300, height=250, bg=background_color)
     Label(window_user, text=user_actual.username, fg='#ffF', bg=background_color).pack()
     Label(window_user, text="", bg=background_color).pack()
-    # boton register
+    # Boton añadir contraseña
     Button(window_user, text="Añadir contraseña", height="2", width="30", bg="#FFFFFF",
            command=añadir_contraseña).pack()
+    # Boton eliminar contraseña
     Button(window_user, text="Eliminar contraseña", height="2", width="30", bg="#FFFFFF",
            command=eliminar_contraseña).pack()
-    # boton log out
-    # Button(window_user, text="Log out", height="2", width="30", bg="#FFFFFF", command=
+    # Boton cerrar sesión
+    Button(window_user, text="Log out", height="2", width="30", bg="#FFFFFF", command=logout).pack()
     window_user.pack()
 
-# login user
+# FUNCION IR PARA ATRAS
+def back():
+    window_register.forget
+    window_login.forget
+    window_home.pack
+
+# FUNCIÓN INICIAR SESIÓN
 def login_user():
+    """Iniciar sesion"""
     logged = user_actual.login_user(username.get(), password.get())
     if logged:
         messagebox.showinfo(message="Sesión iniciada correctamente")
@@ -93,27 +99,36 @@ def login_user():
     else:
         messagebox.showerror(message="Error de inicio de sesión")
 
-# register user
+# FUNCION REGISTRAR USUARIO
 def register_user():
+    """Registrar usuario"""
     try:
         user_actual.register_user(username.get(), password.get())
         messagebox.showinfo(title='Registrado', message="Registrado correctamente")
         window_register.forget()
         user_window()
-
     except ValueError:
         messagebox.showerror(title='Error', message="Nombre de usuario ya registrado")
 
-
+# FUNCION AÑADIR CONTRASEÑA DEL USUARIO
 def añadir_contraseña():
+    """Llama a la funcion añadir contraseña"""
     pass
 
+# FUNCION ELIMINAR CONTRASEÑA DEL USUARIO
 def eliminar_contraseña():
+    """Llama a la funcion eliminar contraseña"""
     pass
 
+# FUNCION CERRAR SESIÓN USUARIO
 def logout():
-    user.__del__()
-
+    """Cerrar sesión"""
+    messagebox.showinfo(message="Sesión cerrada correctamente")
+    user_actual.__del__()
+    window_login.forget()
+    window_user.forget()
+    window_register.forget()
+    window_home.pack()
 
 
 #----CARACTERISTICAS VENTANA----
@@ -135,19 +150,25 @@ user_actual = User()
 window_home = Frame(window_principal)
 window_home.config(width=300, height=250, bg=background_color)
 window_home.pack()
-
 Label(window_home, text="", bg=background_color, fg='#ffF').pack()
+# Boton de iniciar sesión
 Button(window_home, text="Log in", height="2", width="30", bg="#FFFFFF", command=login_window).pack()
 Label(window_home,text="", bg=background_color, fg='#ffF').pack()
+# Boton de registrar
 Button(window_home, text="Register", height="2", width="30", bg="#FFFFFF", command=register_window).pack()
 Label(window_home, text="", bg=background_color, fg='#ffF').pack()
 
+#----VENTANA LOG IN----
 window_login = Frame(window_principal)
 window_login.config(width=300, height=250, bg=background_color)
 
+#----VENTANA REGISTRAR----
 window_register = Frame(window_principal)
 window_register.config(width=300, height=250, bg=background_color)
 
+#----VENTANA USUARIO----
+window_user = Frame(window_principal)
+window_user.config(width=300, height=250, bg=background_color)
 
 window_principal.mainloop()
 
