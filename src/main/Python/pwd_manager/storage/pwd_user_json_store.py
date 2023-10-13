f5018ab4c970f113a52a682a797f27dd2a56244b
RaulCarrero02 <100451286@alumnos.uc3m.es>
2023-10-13 15:32:28 +0200

    Arreglos de inserción de datos en register y login

--- src/main/Python/pwd_manager/storage/pwd_user_json_store.py
+++ src/main/Python/pwd_manager/storage/pwd_user_json_store.py
@@ -1,12 +1,13 @@
 from pwd_manager.cfg.pwd_manager_config import JSON_FILES_PATH
 from .json_store import JsonStore
 
+
 class PwdStore(JsonStore):
     _FILE_PATH = JSON_FILES_PATH
     _ID_FIELD = "user_name"
 
-    def add_item(self, item, id):
-        self._FILE_PATH += str(id)
+    def add_item(self, item, name):
+        self._FILE_PATH += str(name)
         self._FILE_PATH += ".json"
         if not isinstance(item, dict):
             raise ValueError("Invalid object")
